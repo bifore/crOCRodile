9f19fa2b7c5fc84ac11f6f24ca6210433ca15abb
bifore <paul.guenezan@aliceadsl.fr>
2016-10-29 13:01:07 +0200

    ann: add forward prop

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include "io/image.h"
 #include "processing/canny.h"
 
+#include "ann/network.h"
+
 int main()
 {
-    GdkPixbuf *img = img_load("normal_text.bmp");
+    /*GdkPixbuf *img = img_load("normal_text.bmp");
     Canny_filter *cf = canny(img);
     guchar *data = (guchar *) malloc(sizeof(guchar) * cf->w * cf->h * 3);
     int a = 0;
@@ -16,5 +18,22 @@ int main()
     img_save(data, "normal_text_edges.bmp", cf->w, cf->h);
     free(data);
     g_object_unref(img);
-    canny_free(cf);
+    canny_free(cf);*/
+
+    Network *net = net_create(4, 1, "test_1");
+    net_addLayer(net, 10);
+    net_randomizeLayer(net, 0);
+    net_addLayer(net, 1);
+    net_randomizeLayer(net, 1);
+
+    float *input = (float *) malloc(4 * 1 * sizeof(float));
+    input[0] = 1;
+    input[1] = 2;
+    input[2] = 4;
+    input[3] = 6;
+    net_loadInput(net, input);
+
+    net_forward(net);
+
+    net_free(net);
 }
