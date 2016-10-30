49644a7a919a5be7927e1171a4d3d9f8b3717e7a
Tristan Deloche <tristan.deloche@epita.fr>
2016-10-30 14:53:03 +0100

    adding full test image, defaults for it and loading it in main

--- src/defaults.h
+++ src/defaults.h
@@ -6,6 +6,7 @@
 #define TEST_SET_IMAGE_NORMAL_NOSUFFIX      "image_normal"
 #define TEST_SET_IMAGE_LOREM_NOSUFFIX       "image_lorem"
 #define TEST_SET_IMAGE_ROTATION_NOSUFFIX    "image_rotation"
+#define TEST_SET_IMAGE_FULLTEST_NOSUFFIX    "image_fulltest"
 
 #define TEST_SET_OUT_FOLDER "out/"
 
--- src/main.c
+++ src/main.c
@@ -4,12 +4,13 @@
 
 int main()
 {
-    Image *img_binarized = img_load_IMAGE(TEST_SET_FOLDER TEST_SET_IMAGE_ROTATION_NOSUFFIX ".jpg");
+    Image *img_binarized = img_load_IMAGE(TEST_SET_FOLDER TEST_SET_IMAGE_FULLTEST_NOSUFFIX ".jpg");
     img_crop_border(img_binarized, true);
     img_save_IMAGE(
             img_binarized,
-            TEST_SET_FOLDER TEST_SET_OUT_FOLDER TEST_SET_IMAGE_ROTATION_NOSUFFIX ".jpg",
+            TEST_SET_FOLDER TEST_SET_OUT_FOLDER TEST_SET_IMAGE_FULLTEST_NOSUFFIX ".jpg",
             img_binarized->width,
             img_binarized->height
     );
+    return 0;
 }
