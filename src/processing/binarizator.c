b4132d281f348f07a0b1ead30aa24f55285e832d
Tristan Deloche <tristan.deloche@epita.fr>
2016-10-30 14:06:01 +0100

    Simplifying the binarization

--- src/processing/binarizator.c
+++ src/processing/binarizator.c
@@ -3,10 +3,12 @@
 */
 
 #include <limits.h>
+#include <glib/gtypes.h>
 #include "binarizator.h"
-#include "../util/image.h"
 
 int average_color(guchar *p);
+
+guchar *get_point_delta(guchar *orig, int dx, int dy, int row_s, int col_s);
 
 void binarize_around(Image *image,
                      guchar *ori,
@@ -18,18 +20,14 @@ void binarize_around(Image *image,
                      int col_s,
                      float percent_tolerance);
 
-bool is_in_bounds(int x, int y, int len_image, int row_size, int col_size);
-
-guchar *get_point_delta(guchar *orig, int dx, int dy, int row_s, int col_s);
-
 Image *binarize(GdkPixbuf *file, float percent_tolerance)
 {
     printf("Starting binarization");
     Image *img = (Image *) malloc(sizeof(Image));
 
     guchar *origin = gdk_pixbuf_get_pixels(file);
-    int num_bytes = gdk_pixbuf_get_has_alpha(file) ? 4 : 3;
-    int row_size = gdk_pixbuf_get_rowstride(file);
+    int col_s = gdk_pixbuf_get_has_alpha(file) ? 4 : 3;
+    int row_s = gdk_pixbuf_get_rowstride(file);
     img->width = gdk_pixbuf_get_width(file);
     img->height = gdk_pixbuf_get_height(file);
 
@@ -39,25 +37,19 @@ Image *binarize(GdkPixbuf *file, float percent_tolerance)
     img->raster = (bool *) malloc(img->width * img->height * sizeof(bool));
     for (int y = 0; y < img->height; ++y) {
         for (int x = 0; x < img->width; ++x) {
-            guchar *p = origin + y * row_size + x * num_bytes;
+            guchar *p = origin + y * row_s + x * col_s;
             binarize_around(
                     img,
                     p,
                     img->raster,
                     img->width * img->height,
                     y,
-                    row_size,
+                    img->height,
                     x,
-                    num_bytes,
+                    img->width,
                     percent_tolerance
             );
         }
-
-        bool is_true = false;
-        for (int i = 0; i < img->width; ++i) {
-            is_true |= img->raster[y*img->width+i];
-        }
-        //printf("Line %d raster : %s\n",y, is_true ? "true" : "false");
     }
     return img;
 }
@@ -82,28 +74,22 @@ void binarize_around(
     guchar *p_dc = get_point_delta(ori, 0, +1, row_s, col_s);
     guchar *p_dr = get_point_delta(ori, +1, +1, row_s, col_s);
 
-    int ul_bounds = is_in_bounds(x - 1, y - 1, len, row_s, col_s);
+    int ul_bounds = x - 1 >= 0 && y - 1 >= 0;
+    int uc_bounds = y - 1 >= 0;
+    int ur_bounds = x + 1 < col_s && y - 1 > 0;
+    int ml_bounds = x - 1 >= 0;
+    int mr_bounds = x + 1 <= col_s;
+    int dl_bounds = x - 1 >= 0 && y + 1 < row_s;
+    int dc_bounds = y + 1 < row_s;
+    int dr_bounds = x + 1 < col_s && y + 1 < row_s;
+
     int ul = ul_bounds ? average_color(p_ul) : INT_MIN;
-
-    int uc_bounds = is_in_bounds(x, y - 1, len, row_s, col_s);
     int uc = uc_bounds ? average_color(p_uc) : INT_MIN;
-
-    int ur_bounds = is_in_bounds(x + 1, y - 1, len, row_s, col_s);
     int ur = ur_bounds ? average_color(p_ur) : INT_MIN;
-
-    int ml_bounds = is_in_bounds(x - 1, y, len, row_s, col_s);
     int ml = ml_bounds ? average_color(p_ml) : INT_MIN;
-
-    int mr_bounds = is_in_bounds(x + 1, y, len, row_s, col_s);
     int mr = mr_bounds ? average_color(p_mr) : INT_MIN;
-
-    int dl_bounds = is_in_bounds(x - 1, y + 1, len, row_s, col_s);
     int dl = dl_bounds ? average_color(p_dl) : INT_MIN;
-
-    int dc_bounds = is_in_bounds(x, y + 1, len, row_s, col_s);
     int dc = dc_bounds ? average_color(p_dc) : INT_MIN;
-
-    int dr_bounds = is_in_bounds(x + 1, y + 1, len, row_s, col_s);
     int dr = dc_bounds ? average_color(p_dr) : INT_MIN;
 
     int valid = 0;
@@ -123,88 +109,28 @@ void binarize_around(
 
     for (int i = 0; i < 8; ++i)
     {
-        valid += around[i] != INT_MIN ? 1 : 0;
+        valid += around[i] != INT_MIN;
         sum += around[i] != INT_MIN ? around[i] : 0;
     }
 
-    float mean = ((float) sum / (float) valid) - (255 * percent_tolerance);
+    float mean = ((float) sum / (float) valid) - ((float) 255. * percent_tolerance);
 
-    // ul
-    if (around[0] != INT_MIN)
-    {
-        if (ul_bounds)
-        {
-            raster[(y - 1) * row_s + (x - 1)] = ul < mean;
-        }
-    }
-
-    // uc
-    if (around[1] != INT_MIN)
-    {
-        if (uc_bounds)
-        {
-            raster[(y - 1) * row_s + x] = uc < mean;
-        }
-    }
-
-    // ur
-    if (around[2] != INT_MIN)
-    {
-        if (ur_bounds)
-        {
-            raster[(y - 1) * row_s + (x + 1)] = ur < mean;
-        }
-    }
-
-    // ml
-    if (around[3] != INT_MIN)
-    {
-        if (ml_bounds)
-        {
-            raster[y * row_s + (x - 1)] = ml < mean;
-        }
-    }
-
-    // mr
-    if (around[4] != INT_MIN)
-    {
-        if (mr_bounds)
-        {
-            raster[y * row_s + (x + 1)] = mr < mean;
-        }
-    }
-
-    // dl
-    if (around[5] != INT_MIN)
-    {
-        if (dl_bounds)
-        {
-            raster[(y + 1) * row_s + (x - 1)] = dl < mean;
-        }
-    }
-
-    // dc
-    if (around[6] != INT_MIN)
-    {
-        if (dc_bounds)
-        {
-            raster[(y + 1) * row_s + x] = dc < mean;
-        }
-    }
-
-    // dr
-    if (around[7] != INT_MIN)
-    {
-        if (dr_bounds)
-        {
-            raster[(y + 1) * row_s + (x + 1)] = dr < mean;
-        }
-    }
-}
-
-bool is_in_bounds(int x, int y, int len_image, int row_size, int col_size)
-{
-    return x >= 0 && y >= 0 && (y * row_size + x * col_size) < len_image;
+    if (ul_bounds)
+        raster[(y - 1) * row_s + (x - 1)] = ul > mean;
+    if (uc_bounds)
+        raster[(y - 1) * row_s + x] = uc > mean;
+    if (ur_bounds)
+        raster[(y - 1) * row_s + (x + 1)] = ur > mean;
+    if (ml_bounds)
+        raster[y * row_s + (x - 1)] = ml > mean;
+    if (mr_bounds)
+        raster[y * row_s + (x + 1)] = mr > mean;
+    if (dl_bounds)
+        raster[(y + 1) * row_s + (x - 1)] = dl > mean;
+    if (dc_bounds)
+        raster[(y + 1) * row_s + x] = dc > mean;
+    if (dr_bounds)
+        raster[(y + 1) * row_s + (x + 1)] = dr > mean;
 }
 
 
