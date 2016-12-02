ff4a1aa2ef491fd12e258049ef11c49e79b1fc64
Simon Goetz <jivaros@iasus.lan>
2016-12-02 19:06:57 +0100

    new interface (to upgrade)

--- src/interface/interface01.c
+++ src/interface/interface01.c
@@ -1,21 +1,28 @@
-#include <gtk/gtk.h>
-#include <stdio.h>
+# include <gtk/gtk.h>
+# include <stdio.h>
+# include <stdlib.h>
 
-#include "../io/image.h"
+# include "../io/image.c"
 
-static void chooser(GtkWidget *widget, gpointer data)
+GdkPixbuf *img_load(const char *name);
+
+static void detect(GtkWidget *widget, gpointer data)
 {
-    GtkWidget *windowimg;
-    GtkWidget *dialog;
+}
+
+static void learn(GtkWidget *widget, gpointer data)
+{
+}
+
+static void chooser(GtkWidget *widget, gpointer *data)
+{
     GtkWidget *image;
+    GtkWidget *dialog;
     gint res;
 
-    windowimg = gtk_application_window_new(gtk_window_get_application(data));
-    gtk_window_set_title (GTK_WINDOW (windowimg), "Windowimg");
-    gtk_window_set_default_size (GTK_WINDOW (windowimg), 200, 200);
 
     dialog = gtk_file_chooser_dialog_new ("Open File",
-                                          (GtkWindow *) data,
+                                          (GtkWindow *) gtk_window_new(GTK_WINDOW_POPUP),
                                           GTK_FILE_CHOOSER_ACTION_OPEN,
                                           "_Cancel",
                                           GTK_RESPONSE_CANCEL,
@@ -30,34 +37,82 @@ static void chooser(GtkWidget *widget, gpointer data)
         filename = gtk_file_chooser_get_filename (chooser);
         printf("%s\n", filename);
         GdkPixbuf *img = img_load(filename);
+
         img = gdk_pixbuf_scale_simple(img, 960, 540, GDK_INTERP_TILES);
-        image = gtk_image_new_from_pixbuf(img);
+        gtk_image_set_from_pixbuf((GtkImage *) data, img);
     }
-    else
-        image = gtk_image_new();
-    gtk_container_add (GTK_CONTAINER (windowimg), image);
     gtk_widget_destroy (dialog);
-    gtk_widget_show_all (windowimg);
 }
+
 static void 
 activate(GtkApplication *app, 
          gpointer userdata)
 {
     GtkWidget *window; 
     GtkWidget *button;
-    GtkWidget *button_box;
+    GtkWidget *grid;
+    GtkWidget *image;
+    GtkWidget *imagedisplay;
+
+    /* create a new window, and set its title */
 
     window = gtk_application_window_new(app);
     gtk_window_set_title (GTK_WINDOW (window), "Window");
-    gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+    gtk_container_set_border_width (GTK_CONTAINER (window), 50);
 
-    button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
-    gtk_container_add (GTK_CONTAINER (window), button_box);
+    /* Here we construct the container that is going to pack our buttons  */
+    grid = gtk_grid_new ();
 
+    /* Pack the container in the window */  
+ 
+    /* creating image and placing it to the right of the buttons */
+
+    image = gtk_image_new();
+    imagedisplay = gtk_frame_new("");
+    gtk_container_add(GTK_CONTAINER(imagedisplay), image);
+    gtk_grid_attach (GTK_GRID (grid), imagedisplay, 2, 0, 20, 20);
+
+
+    /* creating button load */
+    
     button = gtk_button_new_with_label("Load Image");
-    g_signal_connect(button, "clicked", G_CALLBACK (chooser), window);
-    g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
-    gtk_container_add (GTK_CONTAINER (button_box), button);
+    g_signal_connect(button, "clicked", G_CALLBACK (chooser), image);
+
+    /* Place the load button in the grid cell (0,0), and make it fill
+     * just 1 cell horizontally and vertically (ie no spanning)
+     */
+    
+    gtk_grid_attach (GTK_GRID (grid), button, 0, 0, 1, 1);
+
+    /* creating button detect */
+
+    button = gtk_button_new_with_label ("Detect");
+    g_signal_connect (button, "clicked", G_CALLBACK (detect), NULL);
+
+    /* Place the detect button in the grid cell (0, 1), and make it fill
+     * just 1 cell horizontally and vertically (ie no spanning)
+     */
+
+    gtk_grid_attach (GTK_GRID (grid), button, 0, 2, 1, 1);
+
+    /* creating button learn */
+
+    button = gtk_button_new_with_label ("Learn");
+    g_signal_connect (button, "clicked", G_CALLBACK (learn), NULL);
+
+    /* Place the learn button in the gric cell (0, 2) and make it fill
+     * just 1 cell horizontally and vertically (ie no spanning)
+     */
+
+    gtk_grid_attach (GTK_GRID (grid), button, 0, 4, 1, 1);
+    gtk_container_add (GTK_CONTAINER (window), grid);
+
+    /* Now that we are done packing our widgets, we show them all in 
+     * one go, by calling gtk_widget_show_all() on the window.
+     * This call recusrively calls gtk_widget_show() on all widgets
+     * that are contained in the window, directly or indirectly 
+     */
+
     gtk_widget_show_all (window);
 }
 
@@ -68,9 +123,9 @@ int main(int argc,
     int status;
 
     app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-    g_signal_connect(app, "activate", G_CALLBACK (activate), NULL);
-    status = g_application_run(G_APPLICATION (app), argc, argv);
+    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+    status = g_application_run (G_APPLICATION (app), argc, argv);
     g_object_unref (app);
 
-    return status; 
+    return status;
 }
